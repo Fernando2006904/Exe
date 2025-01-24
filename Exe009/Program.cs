3cc48de624c704d94efe77d7768b031ef76de0b8
Fernando <fernando.lalves@senacsp.edu.br>
2025-01-23 22:17:29 -0300

    Exe46

---
+++ Exe009/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exe009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ladoA, ladoB, ladoc;
+
+            Console.Write("Digite o valor do lado A: ");
+            ladoA = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado B: ");
+
+            ladoB = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o valor do lado C: ");
+
+            ladoc = Convert.ToInt32(Console.ReadLine());
+
+            if ((ladoA < ladoB + ladoc) && (ladoA < ladoB + ladoc) && (ladoc < ladoB + ladoc))
+            {
+                Console.WriteLine("Os valores informados formam um Triângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os valores informados não formam um Triângulo.");
+            }
+
+        }
+    }
+}
