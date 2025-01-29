51d3475ec9bf7c42da543487642deddb8d456f1c
Fernando <fernando.lalves@senacsp.edu.br>
2025-01-28 22:21:41 -0300

    Frutas ínicio

---
+++ Frutas/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Frutas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] frutas = { "Maça", "Banana", "Manga", "Amora" };
+            int[] frutas2 = { 4, 5, 6, 7, 8 };
+        }
+    }
+}
